b43c86dbd7ff9dd4dde67d6fcac77a5f22c3c58d
Xavier Douillard <xavier.douillard@gmail.com>
2019-03-07 15:05:43 +0100

    suppression des fichiers analysis et config de parite.py

--- parite.py
+++ parite.py
@@ -1,8 +1,21 @@
-# /usr/bin/env python3
+#! /usr/bin/env python3
 # coding: utf-8
+import argparse
+
+import analysis.csv as c_an
+import analysis.xml as x_an
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--extension", help="""Type of file to analyse. Is it a CSV or an XML?""")
+    return parser.parse_args()
 
 def main():
-    pass
+    args = parse_arguments()
+    if args.extension == 'xml':
+        x_an.launch_analysis('SyceronBrut.xml')
+    elif args.extension == 'csv':
+        c_an.launch_analysis('current_mps.csv')
 
 if __name__ == "__main__":
     main()
